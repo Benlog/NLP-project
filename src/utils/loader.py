7855067a28d038ea6aec1b45855c44e7759d204c
André Nasturas <andrenasturas@git.delfosia.net>
2022-02-24 16:03:24 +0100

    🏷️ Use pathlib
    Co-authored-by: Benlog <Benlog@users.noreply.github.com>

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -10,24 +10,32 @@ def tokenize_packed(s : str) -> "list(list(int))" :
     '''
     return [[1 if digit=='1' else 0 for digit in f'{ord(i):8b}'] if ord(i) <= 0x7F else [1,1,1,1,1,1,1,1] for i in s]
 
-def tokenize_one_hot(s : str) -> "list(list(int))" :
+
+def tokenize_one_hot(s : str) -> list[list[int]]:
     '''
         return an array of array of 0 or 1 in one hot encoding the position of the caracter in utf8 encoding, if the caracter encoding is no longger than 1 byte, else encode as last position 
     '''
     return [[1 if i == (ord(c) if ord(c) <= 0x7F else 0x80) else 0 for i in range(0x81)] for c in s]
 
+
 def load_json(path : "PathLike") -> "list(dict)" :
     r = []
-    with os.scandir(path) as it:
+    with scandir(path) as it:
         for entry in it:
             if entry.name[-5:] == ".json" and entry.is_file():
                 with open(entry.path, "r", encoding="utf-8") as file :
                     r.append(json.load(file))
     return r
 
-def load_json_iter(path : "PathLike") -> "iter(dict)" :
-    with os.scandir(path) as it:
-        for entry in it:
-            if entry.name[-5:] == ".json" and entry.is_file():
-                with open(entry.path, "r", encoding="utf-8") as file :
+
+def load_json_iter(path: Path) -> Iterator[dict]:
+    # with scandir(path) as it:
+    #     for entry in it:
+    #         if entry.name[-5:] == ".json" and entry.is_file():
+    #             with open(entry.path, "r", encoding="utf-8") as file :
+    #                 yield json.load(file)
+
+    for item in path.iterdir():
+        if item.name[-5:] == ".json" and item.is_file():
+            with item.open("r", encoding="utf-8") as file:
                     yield json.load(file)
