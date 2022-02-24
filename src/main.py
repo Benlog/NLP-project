a998d2dfe059cf8b812d5fa263c36c8682a495cd
André Nasturas <andrenasturas@git.delfosia.net>
2022-02-24 19:47:24 +0100

    ✨ Make DataLoader operational
    Co-authored-by: Benlog <Benlog@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,14 +1,36 @@
-from .utils import JSONDataset
-from dotenv import load_dotenv
-from pathlib import Path
 from os import environ
+from pathlib import Path
+
+import torch
+from dotenv import load_dotenv
 from torch.utils.data import DataLoader
 
-root = Path("..")
-load_dotenv(root)
+from .RRMC import discrimNN
+from .utils import JSONDataset, load_json_iter
 
-data = root / Path(environ.get("DATA_PATH"))
 
-dataset = JSONDataset()
+def main():
+    root = Path(".")
+    load_dotenv(root / Path(".env"))
 
-loader = DataLoader(dataset, batch_size=10, pin_memory=True)
+    data = root / Path(str(environ.get("DATA_PATH"))) / Path("french_articles")
+
+    # max_length = max(len(doc["text"]) for doc in load_json_iter(data))
+    # min_length = min(len(doc["text"]) for doc in load_json_iter(data))
+
+    dataset = JSONDataset(data, min_length=50, vector_length=100)
+
+    loader = DataLoader(dataset, batch_size=10, pin_memory=True)
+
+    loader_iter = iter(loader)
+
+    data1 = next(loader_iter)
+    print(data1.size())
+    print(torch.tensor(data1).size())
+
+    nn = discrimNN(129, 10)
+    ft = nn.forward(data1)
+
+    print(ft)
+
+    print("Done")
--- src/utils/loader.py
+++ src/utils/loader.py
@@ -1,34 +1,44 @@
 import json
+import math
+import torch
 from os import PathLike, scandir
 from pathlib import Path
-from typing import Callable, Iterator, Union
+from typing import Callable, Iterator, Optional, Union
 
 from torch.utils.data import IterableDataset
 
 
-def tokenize_packed(s : str) -> "list(list(int))" :
-    '''
-        return an array of array of 0 or 1 encoding the utf8 code of the caracter, if the caracter encoding is no longger than 1 byte, else encode 0xFF
+def tokenize_packed(s: str) -> list[list[int]]:
+    """
+    return an array of array of 0 or 1 encoding the utf8 code of the caracter, if the caracter encoding is no longger than 1 byte, else encode 0xFF
 
-        f'{ord(i):8b}'
-        give the string representing ord(i) in binary of length 8
-    '''
-    return [[1 if digit=='1' else 0 for digit in f'{ord(i):8b}'] if ord(i) <= 0x7F else [1,1,1,1,1,1,1,1] for i in s]
-
-
-def tokenize_one_hot(s : str) -> list[list[int]]:
-    '''
-        return an array of array of 0 or 1 in one hot encoding the position of the caracter in utf8 encoding, if the caracter encoding is no longger than 1 byte, else encode as last position 
-    '''
-    return [[1 if i == (ord(c) if ord(c) <= 0x7F else 0x80) else 0 for i in range(0x81)] for c in s]
+    f'{ord(i):8b}'
+    give the string representing ord(i) in binary of length 8
+    """
+    return [
+        [1 if digit == "1" else 0 for digit in f"{ord(i):8b}"]
+        if ord(i) <= 0x7F
+        else [1, 1, 1, 1, 1, 1, 1, 1]
+        for i in s
+    ]
 
 
-def load_json(path : "PathLike") -> "list(dict)" :
+def tokenize_one_hot(s: str) -> list[list[int]]:
+    """
+    return an array of array of 0 or 1 in one hot encoding the position of the caracter in utf8 encoding, if the caracter encoding is no longger than 1 byte, else encode as last position
+    """
+    return torch.tensor([
+        [1 if i == (ord(c) if ord(c) <= 0x7F else 0x80) else 0 for i in range(0x81)]
+        for c in s
+    ])
+
+
+def load_json(path: "PathLike") -> list[dict]:
     r = []
     with scandir(path) as it:
         for entry in it:
             if entry.name[-5:] == ".json" and entry.is_file():
-                with open(entry.path, "r", encoding="utf-8") as file :
+                with open(entry.path, "r", encoding="utf-8") as file:
                     r.append(json.load(file))
     return r
 
@@ -43,14 +53,38 @@ def load_json_iter(path: Path) -> Iterator[dict]:
     for item in path.iterdir():
         if item.name[-5:] == ".json" and item.is_file():
             with item.open("r", encoding="utf-8") as file:
+
                 yield json.load(file)
 
 
+def filter_length_json_iter(
+    iterable, min_length=0, max_length=math.inf
+) -> Iterator[str]:
+    for i in iterable:
+        t = i["text"]
+        length = len(t)
+        if length > min_length and length < max_length:
+            yield t
+
+
 class JSONDataset(IterableDataset):
-    def __init__(self, dataset_folder: Union[Path, str], tokenize_fn: Callable = tokenize_one_hot):
+    def __init__(
+        self,
+        dataset_folder: Union[Path, str],
+        min_length: int,
+        vector_length :int,
+        tokenize_fn: Callable = tokenize_one_hot,
+    ):
         super().__init__()
         self.source = Path(dataset_folder)
         self.tokenize_fn = tokenize_fn
+        self.min_length = min_length
+        self.vector_length = vector_length
 
     def __iter__(self):
-        return (self.tokenize_fn(i["text"]) for i in load_json_iter(self.source))
+        return (
+            self.tokenize_fn(i.ljust(self.vector_length, ' '))
+            for i in filter_length_json_iter(
+                load_json_iter(self.source), self.min_length, self.vector_length
+            )
+        )
