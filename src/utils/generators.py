fc80bcda9bf48d8cdd09a5ae0792ad46d69d648a
André Nasturas <andrenasturas@git.delfosia.net>
2022-04-15 18:41:47 +0200

    ✨ Add working main and FakeDataloader
    Co-authored-by: Benlog <Benlog@users.noreply.github.com>

--- src/utils/generators.py
+++ src/utils/generators.py
@@ -2,6 +2,17 @@ from random import choice, randint
 from string import ascii_lowercase
 
 from essential_generators import DocumentGenerator
+from faker import Faker
+
+
+def random_fake_sentences(seed=None):
+    fake = Faker()
+
+    if seed:
+        Faker.seed(seed)
+
+    while True:
+        return fake.sentance()
 
 
 def random_sentences():
@@ -12,11 +23,16 @@ def random_sentences():
 
 def random_characters(length):
     letters = ascii_lowercase
+    words = " ".join(
+        [
+            "".join(choice(letters) for i in range(randint(1, 12)))
+            for j in range(randint(3, length))
+        ]
+    )
+    return words[0].upper() + words[1:] + "."
+
+
+def random_characters_generator(min_length: int, max_length: int):
     while True:
-        words = " ".join(
-            [
-                "".join(choice(letters) for i in range(randint(1, 12)))
-                for j in range(randint(3, length))
-            ]
-        )
-        yield words[0].upper() + words[1:] + "."
+        length = randint(min_length, max_length)
+        yield(random_characters(length))
